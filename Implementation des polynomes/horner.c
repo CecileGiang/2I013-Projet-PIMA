47a37361d651be8fb8d482289a6860662d7b7493
pamelaosuna <33221741+pamelaosuna@users.noreply.github.com>
2018-04-07 21:06:25 +0000

    Update horner.c

--- Implementation des polynomes/horner.c
+++ Implementation des polynomes/horner.c
@@ -7,13 +7,13 @@ void eval_poly_horner(mpz_t *coeff, int a, unsigned int k, unsigned long int deg
     long int d = deg;
     long int i;
     *den = 1;
-    mpz_set(*num, coeff[0]);
-    
-    for (i = d; i > 0; i--){
+
+
+    for (i = d; i >= 0; i--){
       mpz_mul_si(tmp1, *num, a);
       mpz_add(tmp2, coeff[i], tmp1);
       mpz_add(*num, *num, tmp2);
-      *den = (*den)*(1<<k); 		// probleme: a partir d un certain rang, le denominateur vaut 0
+      *den = *den*(1<<k);		// a partir d un certain rang, le denominateur vaut 0
 	//printf("den: %d\n", *den);
   }
       mpz_clear(tmp1);
